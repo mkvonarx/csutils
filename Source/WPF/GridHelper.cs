941eebbadef2f0f5ebe576885c773ad8418d8654
Markus von Arx <markus.von-arx@atos.net>
2020-04-07 08:40:07 +0200

    updated copyright notice

--- Source/WPF/GridHelper.cs
+++ Source/WPF/GridHelper.cs
@@ -3,24 +3,11 @@ using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 
-/* 
- * =========================================================================================================
- * 3-clause BSD license for GridHelper.cs (see http://www.opensource.org/licenses/bsd-license.php)
- * -----------------------------------------------------------------------------------------------
- * Copyright (c) 2011-2014, Markus von Arx <mkvonarx@gmail.com>
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
- * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
- * 3. Neither the name of Markus von Arx nor the names of any contributors may be used to endorse or promote products derived from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * =========================================================================================================
- * 
- * GridHelper.cs Github: https://github.com/mkvonarx/mkvonarx-csutils/blob/master/Source/WPF/GridHelper.cs
- * 
- */
+// Copyright (c) 2011-2020, Markus von Arx
+// All rights reserved.
+// This source code is licensed under the 3-clause BSD license found here https://opensource.org/licenses/BSD-3-Clause 
+
+// GridHelper.cs Github: https://github.com/mkvonarx/mkvonarx-csutils/blob/master/Source/WPF/GridHelper.cs
 
 /* Release Notes:
  *
